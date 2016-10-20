14359df152d70d0b606278b3f64149cb066b0f17
JanGalante <ave_caesar@hotmail.com>
2016-10-20 15:15:19 +0200

    changes in webpack and added some comments

--- src/galante-se/webpack.config.dev.js
+++ src/galante-se/webpack.config.dev.js
@@ -8,24 +8,24 @@ export default {
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index')
+    path.resolve(__dirname, 'wwwroot/index') //path to index.js
   ],
-  target: 'web',
+  target: 'web', //bundle the code so that a web browser can understand it
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
+    publicPath: '/', //the files in dev are in memory, but still need a physical path
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'src')
+    contentBase: path.resolve(__dirname, 'wwwroot') //where to look for the code
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin() //nice error messages in the browser
   ],
-  module: {
+  module: { //types of files we want to handle
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.js$/, include: path.join(__dirname, 'wwwroot'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
